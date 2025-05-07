f7f5da0b4dce0602ae1b5850fa325b9ad82a2f67
Kaish-Tech <erkaish.tech@gmail.com>
2025-05-07 13:21:37 +0530

    added login page

---
+++ FRONTEND/js/login.js
@@ -0,0 +1,64 @@
+import home from "./home.js";
+
+let login = () => {
+    setTimeout(()=>{
+        bindLoginForm()
+    })
+    return `
+    <div class="form-box">
+        <div class="form-container">
+            <h2>Login</h2>
+            <form class="login-form">
+                <div class="form-control">
+                    <input type="text" placeholder="Phone number,username or email" name="email">
+                    <input type="password" placeholder="Password" name="password">
+                    <button type ="submit">Submit</button>
+                </div>
+            </form>
+            <h4>Not Register? <a href="#">Sign up</a></h4>
+        </div>
+    </div>
+    `
+}
+export default login;
+
+export function bindLoginForm(){
+
+    let allInput = document.querySelectorAll(".form-control input")
+    let form = document.querySelector(".login-form")
+    
+    let handleSubmit=(e)=>{
+
+        e.preventDefault()
+        let loginDetails = {}
+        allInput.forEach((inp)=>{
+            loginDetails[inp.name]=inp.value
+        })
+        try {
+            (async () => {
+                const res= await fetch("http://localhost:8080/auth/login",{
+                    method:"POST",
+                    body:JSON.stringify(loginDetails),
+                    headers:{"Content-Type": "application/json"}
+                })
+                const data = await res.json()
+                console.log(data)
+                if(data.token){
+                    alert(`${data.message}`)
+                    window.sessionStorage.setItem("token",`${data.token}`) 
+                    window.sessionStorage.setItem("role",`${data.role}`)
+                    window.history.pushState(null,"","/home")
+                    root.innerHTML = home()
+                    }
+                else{
+                    alert(`Invalid`)
+                }
+
+            })()
+        } catch (error) {
+            console.error("Error : "+error)
+        }
+    }
+    
+    form.addEventListener("submit",handleSubmit)
+}
