44ecefd5df34b7510df4d976349d35797c269b8f
Kaish-Tech <erkaish.tech@gmail.com>
2025-05-08 20:16:46 +0530

    added user profile and logo

--- FRONTEND/js/navbar.js
+++ FRONTEND/js/navbar.js
@@ -9,11 +9,14 @@ import login from "./login.js"
 
 const navbar=()=>{
 	const token = sessionStorage.getItem("token")
-	const role = sessionStorage.getItem("role")
+	const userData = JSON.parse(sessionStorage.getItem("user"))
+	
+
 	let links = `<li><a href="home">Home</a></li>
 	                <li><a href="about" >About</a></li>
 	                <li><a href="#" >Contact</a></li>`
 	if(token){
+		const role = userData.role
 		if(role=== "ADMIN"){
 			links +=`<li><a href="patient" >Patient</a></li>
 	                <li><a href="doctor" >Doctor</a></li>
@@ -30,13 +33,34 @@ const navbar=()=>{
 		else if(role === "STAFF"){
 			links +=`<li><a href="billing" >Billing</a></li>`
 		}
-		links +=`<li><a href="login" >Login</a></li>`
 	}
 	else{
 		links +=`<li><a href="login" >Login</a></li>
 				<li><a href="#" >Signup</a></li>`
 	}
-	return `<nav id ="navbar"><ul>${links}</ul></nav>`
+	let profileSection=``
+	if(token && userData){
+		const initial = userData.name.charAt(0).toUpperCase()
+		profileSection = `
+		<div class="profile-container">
+            <div class="profile-icon">${initial}</div>
+            <div class="profile-dropdown hidden">
+				<p><strong>ID:</strong> ${userData.userId}</p>
+                <p><strong>Name:</strong> ${userData.name}</p>
+                <p><strong>Email:</strong> ${userData.email}</p>
+                <p><strong>Role:</strong> ${userData.role}</p>
+            </div>
+        </div>
+		`
+	}
+	return `<div class ="nav-container">
+	<div class="logo">
+	<h1>EasyMed</h1>
+	</div>
+	<div id="navbar"><ul>${links}</ul></div>
+	${profileSection}
+	
+	</div>`
 }
 
 export default navbar
@@ -72,4 +96,13 @@ export let bindingAllAnchor=()=>{
 	allAnchor.forEach((a)=>{
 		a.addEventListener('click',handleClick)
 	})
+
+
+	const profileIcon = document.querySelector(".profile-icon")
+    const profileDropdown = document.querySelector(".profile-dropdown")
+    if (profileIcon && profileDropdown) {
+        profileIcon.addEventListener("click", () => {
+            profileDropdown.classList.toggle("hidden")
+        })
+    }
 }
